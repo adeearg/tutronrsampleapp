491d0e3557863a4c3ba5a43e7388a8d704e6abb2
Alex R. Garcia <adeegarcia@gmail.com>
2013-07-14 19:08:44 +0800

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
